326f7b3a7a984f09318386b4d1d09921f4ce6106
dastballadares <bal20001@byui.edu>
2021-10-16 15:38:47 -0500

    config email added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,13 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const  sendgridTranport = require('nodemailer-sendgrid-transport');
+
+const transporter = nodemailer.createTransport(sendgridTranport({
+    auth:{
+        api_key:'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',
+    }
+}));
 
 exports.getLogin = (req,res,next)=>{
     console.log(req.session.isLoggedIn)
@@ -88,8 +96,19 @@ exports.postSignup = async (req,res,next)=>{
             password: hashedPassword ,
             cart : { items:[]}
         })
-     await user.save()
-     res.redirect('/login')
+        await user.save();
+        res.redirect('/login')
+        try{
+            
+        await transporter.sendMail({
+                to: email,
+         from: 'pizza@davidpizza.com',
+         subject:'Signup Suceeded',
+         html: '<h1> You successfully signed up</h1>',
+        })
+    }catch( e ){
+        console.log(e)
+    }
     
     }catch(e){
         console.log(e)
